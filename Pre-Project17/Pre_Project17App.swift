c6abd571ed906caf5cf03957d868b579eb06e6e2
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-12-27 14:05:47 -0300

    Initial Commit

---
+++ Pre-Project17/Pre_Project17App.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_Project17App.swift
+//  Pre-Project17
+//
+//  Created by Bruno Oliveira on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Pre_Project17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
